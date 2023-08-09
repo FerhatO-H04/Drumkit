cfb63add10754ca01814f12e86174a86f48c9e7d
FerhatO-H04 <131246514+FerhatO-H04@users.noreply.github.com>
2023-08-09 12:43:52 +0200

    Update drumkit.js
    add "click" function to task drumkit

--- drumkit.js
+++ drumkit.js
@@ -1,17 +1,15 @@
-
+//object 
+const sounds = {
+  j: "Drum1.wav",
+  k: "Drum2.wav",
+  l: "Drum3.wav",
+  a: "Bell1.wav",
+  s: "Bell2.wav",
+  d: "Bell3.wav"
+}
 //arrow function with one parameter and keydown event
 
 window.addEventListener("keydown", (event) => {
-
-//object  
-  const sounds = {
-    j: "Drum1.wav",
-    k: "Drum2.wav",
-    l: "Drum3.wav",
-    a: "Bell1.wav",
-    s: "Bell2.wav",
-    d: "Bell3.wav"
-  }
 
 //variable
   let drumAudio = new Audio (sounds[event.key])
@@ -19,4 +17,33 @@ window.addEventListener("keydown", (event) => {
 //play audio
   drumAudio.play()
 }); 
+
+//returns an array of sounds object
+let allSoundsInArray =  Object.entries(sounds);
+
+
+for( let i = 0; i < allSoundsInArray.length; i++ ){
+  currentSound  = allSoundsInArray[i];
+  
+// use the toUpperCase() method to correspond to the id names in the html file.
+  currentSoundId = currentSound[0].toUpperCase(); 
+  currentSoundValue = currentSound[1]; 
+
+//variables
+  const audio = new Audio (currentSoundValue)
+  const button = document.getElementById(currentSoundId)
+
+//arrow function with click event
+  button.addEventListener("click", () =>{
+  audio.play()
+  })
+  
+ }
+
+
+
+
+
+
+
 
